ffa89923886391a3699ec9c01009d91d264b3b21
Nitin Srivastava <nitinsrivastava2001@gmail.com>
2021-09-01 01:21:54 +0530

    API Tweek

--- Public/js/main.js
+++ Public/js/main.js
@@ -14,7 +14,7 @@ const getInfo=async(event)=>{
        outText.innerText="Plz enter the name of your city";
    }else{
        try{
-    let url=`http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=9fff308d210ba60d6590c42fb75d17d5`;
+    let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=9fff308d210ba60d6590c42fb75d17d5`;
     const response=await fetch(url);
     const data= await response.json();
     const arr=[data];
